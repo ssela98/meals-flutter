5daf9bb43f6718000e18918e1ff89d6582db0c35
Alessandra <tirnavanalessandra@gmail.com>
2023-11-04 16:49:07 +0100

    refactor provider code

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -4,13 +4,17 @@ import 'package:meals/models/meal.dart';
 class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
   FavoriteMealsNotifier() : super([]);
 
-  void toggleMealFavoriteStatus(Meal meal) {
+  bool toggleMealFavoriteStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
 
     if (mealIsFavorite) {
       state = state.where((mealElement) => mealElement.id != meal.id).toList();
+
+      return false;
     } else {
       state = [...state, meal];
+
+      return true;
     }
   }
 }
